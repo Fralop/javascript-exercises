de2149b8ee59c89751e5125cf0eba8cc595424a7
Fralop <117324827+Fralop@users.noreply.github.com>
2024-08-20 11:45:11 -0400

    Had an issue with the for loop condition. It was until the length and not length-1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,20 @@
 const removeFromArray = function() {
     const args = [...arguments];
-    const removeFromArray = args[0];
+    const FromArray = args[0];
     const finalArray = [];
-    for (let i = 1; i < (args.length - 1); i++) {
-       for (let j = 0; j < removeFromArray.length; j++) {
-        if (args[i] === removeFromArray[j]) {
-            delete removeFromArray[j]; 
+    for (let i = 1; i < args.length - 1; i++) {
+       for (let j = 0; j < FromArray.length; j++) {
+        if (args[i] === FromArray[j]) {
+            delete FromArray[j]; 
         }
         }
     }
-    for (let h = 1; h < removeFromArray.length; h++ ) {
-        if (removeFromArray[h] !== undefined) {
-            finalArray.unshift(removeFromArray[h]);
+    for (let h = 1; h < FromArray.length; h++ ) {
+        if (FromArray[h] !== undefined) {
+            finalArray.unshift(FromArray[h]);
         }
     }
+return finalArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
