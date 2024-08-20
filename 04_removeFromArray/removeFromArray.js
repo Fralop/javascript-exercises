3f260dc8d4ba402732bceb89880ea36e4264931d
Fralop <117324827+Fralop@users.noreply.github.com>
2024-08-20 12:12:00 -0400

    Managed to make it work backwords. Still a mistery why args array is modified

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,24 @@
 const removeFromArray = function() {
     const args = [...arguments];
     const FromArray = args[0];
+    const tempArray = FromArray;
     const finalArray = [];
-    for (let i = 1; i < args.length - 1; i++) {
-       for (let j = 0; j < FromArray.length; j++) {
-        if (args[i] === FromArray[j]) {
-            delete FromArray[j]; 
+    for (let i = 1; i < args.length; i++) {
+       for (let j = 0; j < tempArray.length; j++) {
+        if (args[i] === tempArray[j]) {
+            delete tempArray[j]; 
         }
         }
     }
-    for (let h = 1; h < FromArray.length; h++ ) {
-        if (FromArray[h] !== undefined) {
-            finalArray.unshift(FromArray[h]);
+   for (let h = 0; h < tempArray.length; h++ ) {
+        if (tempArray[h] !== undefined) {
+            finalArray.unshift(tempArray[h]);
         }
-    }
+   }
+console.log(args);
+console.log(FromArray);
+console.log(tempArray);
+console.log(finalArray);
 return finalArray;
 };
 // Do not edit below this line
