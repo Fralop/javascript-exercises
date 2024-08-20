932dbd92564e66f2c2dc06f4ff338b462540a31b
Fralop <117324827+Fralop@users.noreply.github.com>
2024-08-20 11:13:31 -0400

    Created the baseline of code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
 const removeFromArray = function() {
+    const args = [...arguments];
+    removeFromArray = args[0];
+    const finalArray = [];
+    for (let i = 1; i < (args.length - 1); i++) {
+       for (let j = 0; j < removeFromArray.length; j++) {
+        if (args[i] === removeFromArray[j]) {
+            delete removeFromArray[j]; 
+        }
+        }
+    }
+    for (let h = 1; h < removeFromArray.length; h++ ) {
+        if (removeFromArray[h] !== undefined) {
+            finalArray.unshift(removeFromArray[h]);
+        }
+    }
 };
 
 // Do not edit below this line
