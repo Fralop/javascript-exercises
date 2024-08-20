1cc1030205de09fbd826e5f24cbcdac75c364def
Fralop <117324827+Fralop@users.noreply.github.com>
2024-08-20 17:25:29 -0400

    created baseline code before the test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let convertedTempC = (tempF - 32) * 5 / 9;
+  let roundedTempC = Math.round(convertedTempC*10)/10;
+  return roundedTempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let convertedTempF = (tempC * 9 / 5)+ 32;
+  let roundedTempF = Math.round(convertedTempF*10)/10;
+  return roundedTempF;
 };
 
 // Do not edit below this line
