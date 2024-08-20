ff638f413f8519909f4a0c2062d1df3c7c5a8380
Fralop <117324827+Fralop@users.noreply.github.com>
2024-08-19 20:56:50 -0400

    First try!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let newArray = [];
+    for (let i = (stringArray.lenght - 1) ; i >= 0 ; i--) {
+        newArray.unshift (stringArray[i]); 
+    } 
+    return newArray;
 };
 
 // Do not edit below this line
