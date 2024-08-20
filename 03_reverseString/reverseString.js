04ce6365f090f3d4a82605f23745a625b45dd812
Fralop <117324827+Fralop@users.noreply.github.com>
2024-08-19 21:10:55 -0400

    LOL it was push instead unshift !

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,7 +2,7 @@ const reverseString = function(string) {
     const stringArray = string.split("");
     const newArray = [];
     for (let i = (stringArray.length - 1) ; i >= 0 ; i--) {
-        newArray.unshift (stringArray[i]); 
+        newArray.push (stringArray[i]); 
     } 
     let newString = newArray.join("");
     return newString;
