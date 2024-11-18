9953909f536b641308f4e62c10290a4df087ee68
Fralop <117324827+Fralop@users.noreply.github.com>
2024-11-18 18:09:34 -0500

    Provided a solution using regular for

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let n = Number(input);
+    if (n>=0) {
+    let fiboArr = [0, 1];
+    for (let i=2; i<=n; i++) {
+        fibArr[i] = fiboArr[i-1] + fiboArr[i-2];
+        }
+    return fiboArr[i];
+    } else return "OOPS";
 };
 
 // Do not edit below this line
