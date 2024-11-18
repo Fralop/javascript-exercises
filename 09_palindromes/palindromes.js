e981f7e7cbc4f022d3a301cad9d6ef4de7faaa9f
Fralop <117324827+Fralop@users.noreply.github.com>
2024-11-17 20:34:35 -0500

    Palindromes solved ...not an easy one!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr =str.split('');
+    let newArr = [];
+    arr.forEach((item, index) => {
+        if (/[a-zA-Z0-9]/.test(item)) {
+            newArr.push(item.toLowerCase());
+        }
+    });
+    let reversedArr = newArr.slice().reverse();
+    let final = newArr.every((item, index) => item == reversedArr[index]);
+    return final;
 };
+
+let result = palindromes("ZZZZ car, a man, a maracaz.");
+console
 
 // Do not edit below this line
 module.exports = palindromes;
+
