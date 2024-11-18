799e97542df19655ef89edcc4007ed629126e900
Fralop <117324827+Fralop@users.noreply.github.com>
2024-11-18 18:56:32 -0500

    Added what I thought it was relevant

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr.forEach(element => {
+        if (element.yearOfDeath === undefined) {
+            const today = new Date();
+            const year = today.getFullYear();
+            element.yearOfDeath = year; 
+        }
+    });
+    arr.sort((a,b) => {
+        return (b.yearOfDeath - b.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth); 
+    })
+    return arr[0];
 };
 
 // Do not edit below this line
