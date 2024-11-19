f781a9c8cd52c23ba41223744ccb660ecaa0be79
Fralop <117324827+Fralop@users.noreply.github.com>
2024-11-18 19:12:30 -0500

    Use forEach and the sort arrays.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,16 +1,17 @@
 const findTheOldest = function(arr) {
     arr.forEach(element => {
         if (element.yearOfDeath === undefined) {
-            const today = new Date();
-            const year = today.getFullYear();
+            const year = new Date().getFullYear();
             element.yearOfDeath = year; 
         }
     });
     arr.sort((a,b) => {
-        return (b.yearOfDeath - b.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth); 
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth); 
     })
-    return arr[0];
+    let result = arr[0];
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
